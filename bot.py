50178cf0cc3e2c0734291e44a8a2c23266f77c13
Earlinn Drow <earlinndrow@gmail.com>
2020-02-29 01:23:36 +0300

    добавлены котики, эмоджи и клавиатура

--- bot.py
+++ bot.py
@@ -1,34 +1,70 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
+from emoji import emojize
+import ephem
+import logging
+from pprint import pprint
+from random import choice
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
-import logging
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
 
-import ephem
-from pprint import pprint
 
 with open(r'C:\projects\mybot\config.py') as config_file:
     api_key = config_file.readline()
 
 
-def greet_user(bot, update):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_data['emo'] = emo
+    text = f'Привет {emo}'
+    my_keyboard = ReplyKeyboardMarkup([
+                                       [
+                                           'Список планет', 
+                                           '/planet', 
+                                           'Котэ'
+                                           ],
+                                       [
+                                           'Полнолуние', 
+                                           '/wordcount',
+                                           'Сменить аватар'
+                                           ]
+                                       ])
+    update.message.reply_text(text, reply_markup=my_keyboard)
 
 
-def talk_to_me(bot, update):
-    user_text = update.message.text 
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = f'Привет {update.message.chat.first_name} {emo}! Ты написала: {update.message.text}'
+    logging.info("User %s, Chat id %s, Message %s", update.message.chat.username,
+                 update.message.chat.id, update.message.text)
     print(user_text)
     update.message.reply_text(user_text)
 
 
-def planet(bot, update):
+def get_user_emo(user_data):
+    user_emoji = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:']
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        user_data['emo'] = emojize(choice(user_emoji), use_aliases=True)
+        return user_data['emo']
+
+
+def change_avatar(bot, update, user_data):
+    if 'emo' in user_data:
+        del user_data['emo']
+    emo = get_user_emo(user_data)
+    update.message.reply_text(f'Новый аватар: {emo}')
+
+
+def planet(bot, update, user_data):
     user_input = update.message.text.split()
     planet, empty_date = user_input[1], 2
     if len(user_input) == empty_date:
@@ -42,14 +78,14 @@ def planet(bot, update):
     update.message.reply_text(constellation)
 
 
-def planets(bot, update):
+def planets(bot, update, user_data):
     all_planets = ephem._libastro.builtin_planets()
     for planet in all_planets:
         print(planet[2])
         update.message.reply_text(planet[2])
 
 
-def wordcount(bot, update):
+def wordcount(bot, update, user_data):
     user_input = update.message.text.split()
     words_num, final_answer, ending = 0, '', ''
     empty_string = 1
@@ -79,7 +115,7 @@ def wordcount(bot, update):
     update.message.reply_text(final_answer)
 
 
-def next_full_moon(bot, update):
+def next_full_moon(bot, update, user_data):
     user_input = update.message.text.split()
     empty_date = 1
     if len(user_input) == empty_date:
@@ -91,17 +127,37 @@ def next_full_moon(bot, update):
     update.message.reply_text(full_moon)
 
 
+def cat_send(bot, update, user_data):
+    cat_list = glob('cats/cat*.jp*g')
+    cat_pic = choice(cat_list)
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+
+
 def main():
     mybot = Updater(api_key, request_kwargs=PROXY)
 
     
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    dp.add_handler(CommandHandler("planet", planet))
-    dp.add_handler(CommandHandler("planets", planets))
-    dp.add_handler(CommandHandler("wordcount", wordcount))
-    dp.add_handler(CommandHandler("next_full_moon", next_full_moon))
+    dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
+
+    dp.add_handler(CommandHandler("planet", planet, pass_user_data=True))
+    #dp.add_handler(RegexHandler('^(Созвездие)$', planet, pass_user_data=True))
+
+    dp.add_handler(CommandHandler("planets", planets, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Список планет)$', planets, pass_user_data=True))
+    
+    dp.add_handler(CommandHandler("wordcount", wordcount, pass_user_data=True))
+    #dp.add_handler(RegexHandler('^(Слова)$', wordcount, pass_user_data=True))
+    
+    dp.add_handler(CommandHandler("next_full_moon", next_full_moon, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Полнолуние)$', next_full_moon, pass_user_data=True))
+    
+    dp.add_handler(CommandHandler("cat", cat_send, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Котэ)$', cat_send, pass_user_data=True))
+
+    dp.add_handler(RegexHandler('^(Сменить аватар)$', change_avatar, pass_user_data=True))
+    
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
 
     mybot.start_polling()
