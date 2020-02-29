07a39165e82e5dd9cb765a73d0d483c0a288893e
Earlinn Drow <earlinndrow@gmail.com>
2020-02-29 21:15:17 +0300

    бот научился присылать gif с котиком, запрашивать контакты и координаты, написана отдеьная функция для клавиатуры

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import ephem
 import logging
 from pprint import pprint
 from random import choice
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
@@ -24,19 +24,7 @@ def greet_user(bot, update, user_data):
     emo = get_user_emo(user_data)
     user_data['emo'] = emo
     text = f'Привет {emo}'
-    my_keyboard = ReplyKeyboardMarkup([
-                                       [
-                                           'Список планет', 
-                                           '/planet', 
-                                           'Котэ'
-                                           ],
-                                       [
-                                           'Полнолуние', 
-                                           '/wordcount',
-                                           'Сменить аватар'
-                                           ]
-                                       ])
-    update.message.reply_text(text, reply_markup=my_keyboard)
+    update.message.reply_text(text, reply_markup=get_keyboard())
 
 
 def talk_to_me(bot, update, user_data):
@@ -45,11 +33,16 @@ def talk_to_me(bot, update, user_data):
     logging.info("User %s, Chat id %s, Message %s", update.message.chat.username,
                  update.message.chat.id, update.message.text)
     print(user_text)
-    update.message.reply_text(user_text)
+    update.message.reply_text(user_text, reply_markup=get_keyboard())
 
 
 def get_user_emo(user_data):
-    user_emoji = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:']
+    user_emoji = [
+                  ':smiley_cat:', 
+                  ':smiling_imp:', 
+                  ':panda_face:', 
+                  ':dog:'
+                  ]
     if 'emo' in user_data:
         return user_data['emo']
     else:
@@ -61,7 +54,10 @@ def change_avatar(bot, update, user_data):
     if 'emo' in user_data:
         del user_data['emo']
     emo = get_user_emo(user_data)
-    update.message.reply_text(f'Новый аватар: {emo}')
+    update.message.reply_text(
+                              f'Новый аватар: {emo}', 
+                              reply_markup=get_keyboard()
+                              )
 
 
 def planet(bot, update, user_data):
@@ -75,14 +71,14 @@ def planet(bot, update, user_data):
     coordinates = body(date)
     constellation = ephem.constellation(coordinates)
     print(constellation)
-    update.message.reply_text(constellation)
+    update.message.reply_text(constellation, reply_markup=get_keyboard())
 
 
 def planets(bot, update, user_data):
     all_planets = ephem._libastro.builtin_planets()
     for planet in all_planets:
         print(planet[2])
-        update.message.reply_text(planet[2])
+        update.message.reply_text(planet[2], reply_markup=get_keyboard())
 
 
 def wordcount(bot, update, user_data):
@@ -112,7 +108,7 @@ def wordcount(bot, update, user_data):
                 ending = 'слов'
             final_answer = f'{answer} {ending}'
     print(final_answer)
-    update.message.reply_text(final_answer)
+    update.message.reply_text(final_answer, reply_markup=get_keyboard())
 
 
 def next_full_moon(bot, update, user_data):
@@ -124,13 +120,64 @@ def next_full_moon(bot, update, user_data):
         date = user_input[1]
     full_moon = ephem.next_full_moon(date)
     print(full_moon)
-    update.message.reply_text(full_moon)
+    update.message.reply_text(full_moon, reply_markup=get_keyboard())
 
 
 def cat_send(bot, update, user_data):
-    cat_list = glob('cats/cat*.jp*g')
-    cat_pic = choice(cat_list)
-    bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+    cat_images = glob('cats/cat*.jp*g')
+    cat_gifs = glob('cats/gif_cats/cat*.*4')
+    cat_pics = cat_images + cat_gifs
+    final_pic = choice(cat_pics)
+    if final_pic in cat_gifs:
+        bot.send_document(
+                          chat_id=update.message.chat.id, 
+                          document=open(final_pic, 'rb'), 
+                          reply_markup=get_keyboard()
+                          )
+    if final_pic in cat_images:
+        bot.send_photo(
+                       chat_id=update.message.chat.id, 
+                       photo=open(final_pic, 'rb'), 
+                       reply_markup=get_keyboard()
+                       )
+
+
+def get_contact(bot, update, user_data):
+    print(update.message.contact)
+    update.message.reply_text(
+                              f'Готово {get_user_emo(user_data)}', 
+                              reply_markup=get_keyboard()
+                              )
+
+
+def get_location(bot, update, user_data):
+    print(update.message.location)
+    update.message.reply_text(
+                              f'Готово {get_user_emo(user_data)}', 
+                              reply_markup=get_keyboard()
+                              )
+
+
+def get_keyboard():
+    contact_button = KeyboardButton('Прислать контакты', request_contact=True)
+    location_button = KeyboardButton('Прислать координаты', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([
+                                        [
+                                           'Список планет', 
+                                           '/planet', 
+                                           'Котэ'
+                                        ],
+                                        [
+                                           'Полнолуние', 
+                                           '/wordcount',
+                                           'Сменить аватар'
+                                        ],
+                                        [
+                                            contact_button, 
+                                            location_button
+                                        ]
+                                      ], resize_keyboard=True)
+    return my_keyboard
 
 
 def main():
@@ -157,6 +204,8 @@ def main():
 
     dp.add_handler(RegexHandler('^(Сменить аватар)$', change_avatar, pass_user_data=True))
     
+    dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
 
